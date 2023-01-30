202c5dce35d3efb3fbda1d0c50abe9b75b0067f3
ishmamAli786 <ishmamali2200@gmail.com>
2023-01-30 21:40:19 +0500

    change router

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  HashRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 import Home from "./module/Home/Home";
 const Routess = () => {
   return (
     <>
-      <Router>
+      <HashRouter>
         <Routes>
           <Route path="/:id?" element={<Home />} />
         </Routes>
-      </Router>
+      </HashRouter>
     </>
   );
 };
